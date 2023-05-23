9d0fcc9decd2c64ffd7f67908753629e526fbb8f
Mateo Estrada <mateoej12@gmail.com>
2023-05-23 11:20:40 -0700

    Adds buttons.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <audio v-if="loading" :src="audioFile" autoplay></audio>
+    <audio v-if="isRevingEngine" :src="revEngine" autoplay></audio>
+    <div class="card">
+      <div class="row">
+        <div class="col">
+          <button
+            @click.prevent="turnOn()"
+            class="btn btn-success rounded-circle m-3 p-3"
+          >
+            Start
+          </button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <button
+            @click.prevent="turnOff()"
+            class="btn btn-danger rounded-circle m-3 p-3"
+          >
+            Off
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col">
+          <button
+            @click.prevent="revingEngine()"
+            class="btn btn-warning rounded-circle m-3 p-3"
+          >
+            Rev
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+import audioFile from "@/assets/supercar_engine_start.wav";
+import revEngine from "@/assets/supercar_rev.wav";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      loading: false,
+      audioFile: audioFile,
+      revEngine: revEngine,
+      isRevingEngine: false,
+    };
+  },
+  methods: {
+    turnOn() {
+      this.loading = true;
+    },
+    turnOff() {
+      this.loading = false;
+    },
+    revingEngine() {
+      this.isRevingEngine = true;
+    },
   },
 };
 </script>
