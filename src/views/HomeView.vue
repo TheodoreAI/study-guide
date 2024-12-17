7c359f21f8d8776760142feacff95cf62483097c
Mateo <mateo.estrada@oregonstate.edu>
2024-12-16 16:52:32 -0800

    sets up working environment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,72 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-
-    <audio v-if="loading" :src="audioFile" autoplay></audio>
-    <audio v-if="isRevingEngine" :src="revEngine" autoplay></audio>
-    <div class="card">
-      <div class="row">
-        <div class="col">
-          <button
-            @click.prevent="turnOn()"
-            class="btn btn-success rounded-circle m-3 p-3"
-          >
-            Start
-          </button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <button
-            @click.prevent="turnOff()"
-            class="btn btn-danger rounded-circle m-3 p-3"
-          >
-            Off
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="row">
-        <div class="col">
-          <button
-            @click.prevent="revingEngine()"
-            class="btn btn-warning rounded-circle m-3 p-3"
-          >
-            Rev
-          </button>
-        </div>
-      </div>
-    </div>
+  <div>
+    <h1>Home View</h1>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-
-import audioFile from "@/assets/supercar_engine_start.wav";
-import revEngine from "@/assets/supercar_rev.wav";
 export default {
   name: "HomeView",
-  components: {},
   data() {
     return {
-      loading: false,
-      audioFile: audioFile,
-      revEngine: revEngine,
-      isRevingEngine: false,
+      msg: "Welcome to Your Vue.js App",
     };
-  },
-  methods: {
-    turnOn() {
-      this.loading = true;
-    },
-    turnOff() {
-      this.loading = false;
-    },
-    revingEngine() {
-      this.isRevingEngine = true;
-    },
   },
 };
 </script>
