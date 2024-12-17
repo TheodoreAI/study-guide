30c60a2af5dc840302dd0ea7415e3d90835cc551
Mateo <mateo.estrada@oregonstate.edu>
2024-12-16 17:03:49 -0800

    selecting study guide type

--- src/views/StudyGuideHome.vue
+++ src/views/StudyGuideHome.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="card">
-    <h1>Study Guide</h1>
-    <p>You will be creating a study guide here</p>
+  <div class="container-fluid">
+    <div class="card m-1 p-3">
+      <h1>Study Guide</h1>
+      <p>You will be creating a study guide here.</p>
+    </div>
+
+    <div class="card m-1 p-3">
+      <h2>Select a Study Guide type</h2>
+      <p>Here you can select the specific kind of study guide:</p>
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Select Study Guide Type
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Flash Cards</a>
+          <a class="dropdown-item" href="#">Multiple Choice</a>
+          <a class="dropdown-item" href="#">True/False</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -10,7 +34,7 @@ export default {
   name: "StudyGuideHomeView",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
+    
     };
   },
 };
